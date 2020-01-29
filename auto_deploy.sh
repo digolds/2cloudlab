1a19966fa8dd033edf8542119f64bf865009fc9b
郑思龙 <zhengsilong@zhengsilongdeMacBook-Pro.local>
2020-01-29 09:56:36 +0800

    add

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+echo "Script executed from: ${PWD}"
+
 #push to git
+echo "push to hugo material"
 git add .
 git commit -m $1
 git push origin
 
 #hugo generate static file
+echo "generate static site by hugo"
 hugo -D -d ../p2cloudlab/
 
 #push static web site to git
+echo "push static web site to git"
 cd ../p2cloudlab/
 git add .
 git commit -m $1
